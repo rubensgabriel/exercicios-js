104d6017d43b29f32faba845ac6c521139011abb
Rubens Gabriel <rubens-ita@hotmail.com.br>
2024-04-21 15:45:27 -0300

    conclusao dos fundamentos

---
+++ fundamentos/erro.js
@@ -0,0 +1,23 @@
+function tratarErroELancar(erro) {
+    // throw new Error('...')
+    // throw 10
+    // throw true
+    // throw 'mensagem'
+    throw {
+        nome: erro.name,
+        msg: erro.mrssage,
+        date: new Date
+    }
+}
+function imprimirNomeGritado(obj) {
+    try {
+        console.log(obj.name.toUpperCase() + '!!!')
+    } catch (e) {
+        tratarErroELancar(e)
+    } finally {
+        console.log('final')
+    }
+}
+
+const obj = { name: 'Roberto' }
+imprimirNomeGritado(obj)
